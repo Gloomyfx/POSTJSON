f39fc8fb5eee8b75d8f8b7ec0f3fdf55e2ec5298
Gloomyfx <radityaprananda@rocketmail.com>
2015-08-08 15:10:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/raditya/mainscreenactivity/AllProductsActivity.java
@@ -0,0 +1,194 @@
+package com.example.raditya.mainscreenactivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+public class AllProductsActivity extends ListActivity {
+    // Progress Dialog
+    private ProgressDialog pDialog;
+
+    //Creating JSON Parser object
+    JSONParser jParser = new JSONParser();
+
+    ArrayList<HashMap<String, String>> productsList;
+
+    // url to get all products list
+    private static String url_all_products = "http://api.androidhive.info/android_connect/get_all_products.php";
+
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_DATA = "data";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAMA = "nama";
+
+    // products JSONArray
+    JSONArray data = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.all_products);
+
+        //hashmap for listview
+        productsList = new ArrayList<HashMap<String, String>>();
+
+        //Loading products in Background Thread
+        new LoadAllProducts().execute();
+
+        // Get listView
+        ListView lv = getListView();
+
+        // on seleting single product
+        // launching Edit Product Screen
+        lv.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // getting values from selected ListItem
+                String Id = ((TextView) view.findViewById(R.id.id)).getText()
+                        .toString();
+
+                // Starting new intent
+                Intent in = new Intent(getApplicationContext(),
+                        EditProductActivity.class);
+                // sending pid to next activity
+                in.putExtra(TAG_ID, Id);
+
+                // starting new activity and expecting some response back
+                startActivityForResult(in, 100);
+            }
+        });
+    }
+
+    // Response from Edit Product Activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+     super.onActivityResult(requestCode,resultCode,data);
+        //if result code 100
+        if (resultCode == 100){
+            Intent intent = getIntent();
+            finish();
+            startActivity(intent);
+
+        }
+    }
+
+    class LoadAllProducts extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(AllProductsActivity.this);
+            pDialog.setMessage("Loading products. Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * getting All products from url
+         * */
+        protected String doInBackground(String... args) {
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            // getting JSON string from URL
+            JSONObject json = jParser.makeHttpRequest(url_all_products, "GET", params);
+
+            // Check your log cat for JSON reponse
+            Log.d("All Products: ", json.toString());
+
+            try {
+                // Checking for SUCCESS TAG
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // products found
+                    // Getting Array of Products
+                    data = json.getJSONArray(TAG_DATA);
+
+                    // looping through All Products
+                    for (int i = 0; i < data.length(); i++) {
+                        JSONObject c = data.getJSONObject(i);
+
+                        // Storing each json item in variable
+                        String Id = c.getString(TAG_ID);
+                        String nama = c.getString(TAG_NAMA);
+
+                        // creating new HashMap
+                        HashMap<String, String> map = new HashMap<String, String>();
+
+                        // adding each child node to HashMap key => value
+                        map.put(TAG_ID, Id);
+                        map.put(TAG_NAMA, nama);
+
+                        // adding HashList to ArrayList
+                        productsList.add(map);
+                    }
+                } else {
+                    // no products found
+                    // Launch Add New product Activity
+                    Intent i = new Intent(getApplicationContext(),
+                            NewProductActivity.class);
+                    // Closing all previous activities
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+            // updating UI from Background Thread
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    /**
+                     * Updating parsed JSON data into ListView
+                     * */
+                    ListAdapter adapter = new SimpleAdapter(
+                            AllProductsActivity.this, productsList,
+                            R.layout.list_item, new String[] { TAG_ID,
+                            TAG_NAMA},
+                            new int[] { R.id.id, R.id.nama });
+                    // updating listview
+                    setListAdapter(adapter);
+                }
+            });
+
+        }
+
+    }
+}
+
+
---
+++ app/src/main/java/com/example/raditya/mainscreenactivity/NewProductActivity.java
@@ -0,0 +1,123 @@
+package com.example.raditya.mainscreenactivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class NewProductActivity extends Activity {
+
+    // Progress Dialog
+    private ProgressDialog pDialog;
+    JSONParser jsonParser = new JSONParser();
+    EditText inputId;
+    EditText inputNama;
+    EditText inputLatitude;
+    EditText inputLongitude;
+
+    // url to create new product
+    private static String url_create_product = "http://api.androidhive.info/android_connect/create_product.php";
+
+    //JSON Node Names
+    private static final String TAG_SUCCESS = "success";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_product);
+
+        //Edit Text
+        inputId = (EditText) findViewById(R.id.id);
+        inputNama = (EditText) findViewById(R.id.nama);
+        inputLatitude = (EditText) findViewById(R.id.latitude);
+        inputLongitude = (EditText) findViewById(R.id.longitude);
+
+        //Create Button
+        Button btnSave = (Button) findViewById(R.id.btnSave);
+
+        //button click event
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new CreateNewProduct().execute();
+
+            }
+        });
+
+    }
+
+    class CreateNewProduct extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(NewProductActivity.this);
+            pDialog.setMessage("Creating Product...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+
+
+        }
+
+        protected String doInBackground(String... args) {
+            String id = inputId.getText().toString();
+            String nama = inputNama.getText().toString();
+            String latitude = inputLatitude.getText().toString();
+            String longitude = inputLongitude.getText().toString();
+
+            //Building parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("id", id ));
+            params.add(new BasicNameValuePair("nama", nama));
+            params.add(new BasicNameValuePair("latitude", latitude));
+            params.add(new BasicNameValuePair("longitude", longitude));
+
+            // getting JSON Object
+            // Note that create product url accepts POST method
+
+            JSONObject json = jsonParser.makeHttpRequest(url_create_product, "POST", params);
+
+            //Check log cat from response
+            Log.d("Create Response", json.toString());
+
+            // check for success tag
+            try {
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    //successfully created product
+                    Intent i = new Intent(getApplicationContext(), AllProductsActivity.class);
+                    startActivity(i);
+
+                    finish();
+
+                } else {
+                    // failed to create product
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+    }
+
+    protected void onPostExecute(String file_url) {
+        pDialog.dismiss();
+    }
+
+}
+
